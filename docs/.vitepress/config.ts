44a8e96a2112809c78e574c77ea5a77cb33deaf0
Rahul Rumalla <rahulrumalla@gmail.com>
2023-08-01 10:42:59 +0200

    Updates to config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,8 +4,13 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "Gumdrop 🍭",
   description:
-    "Gumdrop is a tool for web3 communities to create and distribute Non-Transferable Badges to incentivize their members",
-  lastUpdated: true,
+    "Gumdrop is a tool for web3 builders, teams and organizations to distribute Non-Transferable Badges to reward, recognize and incentivize humans",
+  lastUpdated: false,
+  lang: "en-US",
+  cleanUrls: true,
+  head: [
+
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
@@ -13,25 +18,27 @@ export default defineConfig({
       // { text: "Blog", link: "https://mirror.otterspace.xyz" },
     ],
 
-    sidebar: [
-      {
-        text: "Examples",
-        items: [
-          { text: "Markdown Examples", link: "/markdown-examples" },
-          { text: "Runtime API Examples", link: "/api-examples" },
-        ],
-      },
-    ],
+    // sidebar: [
+    //   {
+    //     text: "Examples",
+    //     items: [
+    //       { text: "Markdown Examples", link: "/markdown-examples" },
+    //       { text: "Runtime API Examples", link: "/api-examples" },
+    //     ],
+    //   },
+    // ],
 
     socialLinks: [
-      { icon: "twitter", link: "https://github.com/gumdrop-wtf" },
+      { icon: "twitter", link: "https://twitter.com/rahulrumalla" },
       { icon: "github", link: "https://github.com/gumdrop-wtf" },
-      { icon: "discord", link: "https://github.com/gumdrop-wtf" },
+      // { icon: "discord", link: "https://github.com/gumdrop-wtf" },
     ],
 
     footer: {
-      message: "Gumdrop is a community-owned app, forked from Otterspace under the MIT License.",
-      copyright: "Copyright © 2019-present Evan You",
+      message:
+        "Gumdrop is a community-owned app, forked from Otterspace under the MIT License.",
+      copyright:
+        "Made with ❤️ by <a style='text-decoration: underline; color: blue' href='https://twitter.com/rahulrumalla' target='_blank'>rsquare</a>",
     },
   },
 });
